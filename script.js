0459517ed9f16cd2e0498b646fc49fc35ab63710
Akshay Rawat <rawatakshay428@gmail.com>
2026-02-20 17:32:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-//your JS code here. If required.
 const codes = document.querySelectorAll(".code");
 
 codes[0].focus();
 
 codes.forEach((code, idx) => {
 
-    // forward typing
     code.addEventListener("input", () => {
         code.value = code.value.replace(/[^0-9]/g, "");
 
@@ -14,10 +12,15 @@ codes.forEach((code, idx) => {
         }
     });
 
-    // backspace behavior
     code.addEventListener("keydown", (e) => {
         if (e.key === "Backspace") {
-            if (!code.value && idx > 0) {
+
+            // if current has value → clear only
+            if (code.value) {
+                code.value = "";
+            }
+            // if empty → move focus back
+            else if (idx > 0) {
                 codes[idx - 1].focus();
             }
         }
